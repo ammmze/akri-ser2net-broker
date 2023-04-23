ba6f5eb50adfc95ff8cc224ed330e69deb8863c1
Branden Cash <203336+ammmze@users.noreply.github.com>
2023-04-22 20:44:08 -0700

    feat: support for new env format in akri 0.10.4

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env sh
 set -eux
 
-if [ -z "${UDEV_DEVNODE}" ]; then
-    echo 'UDEV_DEVNODE environment variable was not set'
+env_name=$(env | grep -o -e '^UDEV_DEVNODE\(_[A-F0-9]\{6\}\)\?' | head -n1)
+
+if [ -z "${env_name}" ]; then
+    echo 'No UDEV_DEVNODE or UDEV_DEVNODE_{instance id} environment variable was present'
     exit 1
 fi
+
+UDEV_DEVNODE="$(printenv "${env_name}")"
+
+echo "Using device: ${env_name}=${UDEV_DEV_NODE}"
 
 exec ser2net -d -C "${PORT}:${STATE}:${TIMEOUT}:${UDEV_DEVNODE}:${DEVICE_OPTIONS}"
